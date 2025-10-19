bbb291348e26ce8fdfcf35d38fa2eef013b0943e
R4L34 <ruslan1969427@gmail.com>
2025-10-20 00:59:21 +0300

    Delete Main.java

--- src/main/java/com/r4l/Main.java
+++
@@ -1,9 +0,0 @@
-package com.r4l;
-
-import com.r4l.simple_2d_engine.Engine;
-
-public class Main {
-	public static void main(String[] args) {
-		Engine.getInstance().drawScreen(new TestScreen());
-	}
-}
